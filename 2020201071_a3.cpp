5a79cda8c54e2a1c1aa9bb21a2ab546353df55a5
ritvik-garg <ritvik.garg@students.iiit.ac.in>
2021-02-19 22:21:01 +0530

    done with the code, added readme

--- 2020201071_a3.cpp
+++ 2020201071_a3.cpp
@@ -12,7 +12,6 @@ class Node{
         int sizeOfNode=0;
         bool is_leaf = true;
         unordered_map<int, int> count_keys;
-        friend class BPlusTree;
 };
 
 class BPlusTree{
@@ -23,17 +22,21 @@ class BPlusTree{
             root=NULL;
         }
         void insert(int);
-        void search(int key);
+        bool search(int key);
         void helperInsertInternal(Node* current, Node* child, int value);
         Node* getParent(Node* root, Node* currNode);
+        int getCount(int key);
+        int rangeQuery(int a, int b);
 };
 
 void BPlusTree::insert(int key){
+    // cout << "\n inserting " << key << endl;
     if(root==NULL){
         root = new Node;
         root->is_leaf = true;
         root->key[0] = key;
         root->sizeOfNode=1;
+        root->count_keys[key]++;
     }
     else{
         Node* currNode = root;
@@ -56,31 +59,36 @@ void BPlusTree::insert(int key){
             }
         }
 
+        if(currNode->count_keys.find(key) != currNode->count_keys.end()){
+                currNode->count_keys[key]++;
+                // cout << "Key already Inserted\n";
+                return;
+        }   
+
         if(currNode->sizeOfNode < max_keys){
 
             // unordered_map<int, int> m2 = currNode->count_keys;
-            if(currNode->count_keys.find(key) != currNode->count_keys.end()){
-                currNode->count_keys[key]++;
-                cout << "Key Inserted\n";
-                return;
-            }
+            
 
             int i;
             for(i=0; i<currNode->sizeOfNode; i++){
                 if(currNode->key[i] >= key)
                     break;
             }
-            currNode->key[i] = key;
             for(int index = currNode->sizeOfNode; index>i; index--){
                 currNode->key[index] = currNode->key[index-1];
             }
+            currNode->key[i] = key;
 
             currNode->sizeOfNode+=1;
             int s = currNode->sizeOfNode;
             currNode->ptr[s] = currNode->ptr[s-1];
             currNode->ptr[s-1] = NULL;
-            currNode->count_keys[key]++;
-            
+            currNode->count_keys[key]++;  
+            // cout << "here\n";
+            // for (auto i : currNode->count_keys){
+            //     cout << i.first << " : " << i.second << endl;    
+            // }          
         }
         else{
             Node* newLeafNode = new Node;
@@ -94,21 +102,48 @@ void BPlusTree::insert(int key){
             sort(allKeys.begin(), allKeys.end());
 
             currNode->sizeOfNode = (max_keys+1)/2;
-            newLeafNode->sizeOfNode = max_keys+1 -(max_keys+1)/2;
+            newLeafNode->sizeOfNode = (max_keys+1 -((max_keys+1)/2));
 
             int i;
+            
             for(i=0;i<currNode->sizeOfNode; i++){
                 currNode->key[i] = allKeys[i];
+                if(allKeys[i]==key){
+                    currNode->count_keys[key]++;
+                }
             }
             for(int j=0;j<newLeafNode->sizeOfNode;j++){
                 newLeafNode->key[j] = allKeys[i];
+                
+                if(allKeys[i]!=key){
+                    newLeafNode->count_keys[allKeys[i]] = currNode->count_keys[allKeys[i]];
+                    currNode->count_keys.erase(allKeys[i]);
+                }
+                else
+                    newLeafNode->count_keys[allKeys[i]]++;
                 i++;
             }
+
+            // cout << "here2\n";
+            // for (auto i : currNode->count_keys){
+            //     cout << i.first << " : " << i.second << endl;    
+            // }  
+
+            // cout << "here3\n";
+            // for (auto i : newLeafNode->count_keys){
+            //     cout << i.first << " : " << i.second << endl;    
+            // }  
 
             currNode->ptr[currNode->sizeOfNode] = newLeafNode;
             newLeafNode->ptr[newLeafNode->sizeOfNode] = currNode->ptr[max_keys];
             currNode->ptr[max_keys] = NULL;
             newLeafNode->is_leaf = true;
+
+            // cout << "printing ptrs :\n";
+            // for(int i=0;i<=currNode->sizeOfNode; i++){
+            //     if(currNode->ptr[i]!=NULL)
+            //         cout << "yeh yaha i hai : " << i << endl;
+            // }
 
             if(currNode==root){
                 Node* temp = new Node;
@@ -232,10 +267,10 @@ Node* BPlusTree::getParent(Node* currNode, Node* child){
     return parent;
 }
 
-void BPlusTree:: search(int key){
+bool BPlusTree:: search(int key){
     if(!root){
         cout << "B+ Tree is empty\n";
-        return;
+        return false;
     }
 
     Node* currNode = root;
@@ -254,21 +289,167 @@ void BPlusTree:: search(int key){
 
     for(int i=0;i<currNode->sizeOfNode;i++){
         if(currNode->key[i]==key){
-            cout << "key found\n";
-            return;
+            // cout << "key found\n";
+            return true;
         }
     }
-    cout << "key not found\n";
-    return;
+    // cout << "key not found\n";
+    return false;
 }
 
+int BPlusTree :: getCount(int key){
+    if(!root){
+        cout << "B+ Tree is empty/n";
+        return INT_MIN;
+    }
 
+    Node* currNode = root;
+    while(!currNode->is_leaf){
+        for(int i=0; i<currNode->sizeOfNode; i++){
+            if(currNode->key[i] > key){
+                currNode = currNode->ptr[i];
+                break;
+            }
+            if(i==currNode->sizeOfNode-1){
+                currNode = currNode->ptr[i+1];
+                break;
+            }
+        }
+    }
 
-int main(){
+    // int ind=0;
+    // cout << "here\n";
+    // for (auto i : currNode->count_keys){
+    //     cout << i.first << " : " << i.second << endl;    
+    // }
+
+    for(int i=0;i<currNode->sizeOfNode;i++){
+        // cout << "keys : " << currNode->key[i] << endl;
+        if(currNode->key[i]==key){
+            // cout << "key found\n";
+            return (currNode->count_keys)[key];
+        }
+    }
+    // cout << "key not found\n";
+    return 0;
+}
+
+int BPlusTree :: rangeQuery(int a, int b){
+    if(!root){
+        cout << "B+ Tree is empty\n";
+        return INT_MIN;
+    }
+
+    Node* currNode = root;
+    while(!currNode->is_leaf){
+        currNode = currNode->ptr[0];
+    }
+    // cout << "size of ode : " << currNode->sizeOfNode << endl;
+    int count=0, i=0;
+    // cout << "in range queries : " << endl;
+    // for(int j=0;j<=currNode->sizeOfNode; j++){
+    //     cout << "i : " << j << endl;
+    //     cout << currNode->key[j] <<endl;
+    //     cout << currNode->ptr[j] << endl;
+    //     cout << endl;
+    // }
+
+    // cout << "\nafs\n";
+    i=0;
+    while(currNode->ptr[currNode->sizeOfNode]!=NULL){
+        // cout << "inside while \n";
+        // cout << currNode->key[i] << endl;
+        if(currNode->key[i]>=a && currNode->key[i]<=b)
+            count += currNode->count_keys[currNode->key[i]];
+        i++;
+        if(i==currNode->sizeOfNode){
+            i=0;
+            currNode = currNode->ptr[currNode->sizeOfNode];
+        }
+    }
+    Node* lastLeafNode = currNode;
+
+    for(int i=0; i<lastLeafNode->sizeOfNode;i++){
+        if(lastLeafNode->key[i]>=a && lastLeafNode->key[i]<=b)
+            count += lastLeafNode->count_keys[lastLeafNode->key[i]];
+    }
+    return count;
+}
+
+int main(int argc, char *argv[]){
+    
     BPlusTree bplustree;
-    bplustree.insert(1);
-    bplustree.insert(2);
-    bplustree.insert(3);
-    bplustree.search(7);
+    
+    string ipFile = argv[1];
+    string opFile = "output.txt";
+    // cout << "input filename : " << ipFile << endl;
+    string query;
+    fstream f;
+    fstream f2;
+    f2.open(opFile, ios::out);
+    f.open(ipFile, ios::in);
+    string word;
+    bool res = true;
+
+    int count, range;
+    string a,b;
+
+    while(getline (f, query)){
+        // cout << query << endl;
+        istringstream q(query);
+        while(q >> word){
+            // cout << "word : " << word << endl;
+            if(word=="INSERT"){
+                q >> word;
+                bplustree.insert(stoi(word));
+            }
+            else if(word=="FIND"){
+                q >> word;
+                res = bplustree.search(stoi(word));
+                if(res)
+                    f2 << "YES\n";
+                else
+                    f2 << "NO\n";
+            }
+            else if(word=="COUNT"){
+                q >> word;
+                count = bplustree.getCount(stoi(word));
+                f2 << to_string(count)+"\n";
+            }
+            else if(word=="RANGE"){
+                q >> a;
+                q >> b;
+                range = bplustree.rangeQuery(stoi(a), stoi(b));
+                f2 << to_string(range)+"\n";
+            }
+            else{
+                f2 << "invalid query : sytax errror\n";
+            }
+        }
+    }
+
+    f.close();
+    f2.close();
+
+     
+    // while()
+    // BPlusTree bplustree;
+    // bplustree.insert(1);
+    // bplustree.insert(2);
+    // bplustree.insert(3);
+    // bplustree.insert(3);
+    // bplustree.insert(3);
+    // bplustree.insert(5);
+    // bplustree.insert(3);
+    // bplustree.insert(4);
+    // bplustree.search(9);
+    // cout << "count : " <<  bplustree.getCount(6) << endl;
+    // bplustree.insert(9);
+    // bplustree.search(6);
+    // cout << "count : " <<  bplustree.getCount(6) << endl;
+    // cout << "count : " << bplustree.getCount(3) << endl;
+    // cout << "count : " << bplustree.getCount(5) << endl;
+
+    // cout << bplustree.rangeQuery(2,9);
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,20 @@
+## **DS : Assignment-3 (B+ Tree)**
+
+#### Files : 
+* 2020201071_a3.cpp
+* README.md
+
+#### How to run ?
+1. Compile C++ file using command : `g++ 2020201071_a3.cpp`
+2. Run the binary file. Also give the `name of file containing queries` as command line arguments, like `./a.out input.txt`.
+
+#### Output :
+`output.txt` file is generated containing the results of all the queries in each line.
+
+#### Note :
+1. Query should be in `UPPER-CASE`, eg : INSERT, FIND, COUNT, RANGE.
+2. Order of the tree : 2, i.e., max_keys = 2, and max_child = 3;
+3. Code is implemented in `C++`. 
+4. Keys to be stored should only be `integers`.
+5. Valid Query and its parameter should be passed in the file, otherwise the code may break.
+
